e3c86b0f3052de009e6a8257506618a2aae444c5
Phenom <royswarnava5@gmail.com>
2023-11-30 20:50:51 +0530

    add sitemap csv and write to csv

--- basic_sitemap.py
+++ basic_sitemap.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 import xml.etree.ElementTree as ET
 
 def get_sitemap_urls(url):
@@ -16,6 +17,12 @@ def get_sitemap_urls(url):
         print(f"Error: Unable to retrieve sitemap from {url}")
         return None
 
+def write_to_csv(file_path, data):
+    with open(file_path, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(["Sitemap URLs"])
+        csv_writer.writerows([[url] for url in data])
+
 # Example usage
 website_url = "https://1mg.com"
 sitemap_urls = get_sitemap_urls(f"{website_url}/sitemap.xml")
@@ -24,3 +31,8 @@ if sitemap_urls:
     print("Sitemap URLs:")
     for url in sitemap_urls:
         print(url)
+
+    # Write URLs to CSV file
+    csv_file_path = "sitemap_urls.csv"
+    write_to_csv(csv_file_path, sitemap_urls)
+    print(f"\nSitemap URLs written to {csv_file_path}")
